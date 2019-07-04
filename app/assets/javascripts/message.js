aead910511529c0f5881afbf8768bd704e2d2087
egawasadanori <egawasadanori@egawasadanorinoMacBook-puro.local>
2019-07-04 17:12:22 +0900

    Edit json.builder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,46 @@
 $(function(){
-  $().on('submit', function(e){
+  function buildMessage(message){
+     var html = `<div class="message">
+                    <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.name}
+                  </div>
+                  <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                    </div>
+                    <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    
+                    </div>
+                 </div>`
+    return html;
+  }
 
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+     var formData = new FormData(this);
+    //  console.log(formData);
+     var url = $(this).attr('action');
+     $.ajax({
+       url: url,
+       type: "POST",
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+    })
+    .done(function(message){
+      console.log(message)
+      var html = buildMessage(message);
+      $('.messages').append(html)
+      $('#message_content').val('')
+      $('input').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('エラー')
+    })
   })
-});
+ });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+# binding.pry
+json.content @message.content
+json.name @message.user.name
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
