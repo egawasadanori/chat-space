1de9afc1eb45e8c667d3ef2d79ec46b57bf1307b
egawasadanori <egawasadanori@egawasadanorinoMacBook-puro.local>
2019-07-16 21:08:42 +0900

    Edit user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -30,43 +30,34 @@ $(function(){
         var html =buildUser(user);
         $('#user-search-result').append(html);
         // $('#user-search-result').val('');
-        $('#user-search-result').find('#user-search-result').remove();
+        // $('#user-search-result').find('#user-search-result').remove();
+        $(this).parent().remove();
+
        })
       })
       .fail(function(){
         alert('ユーザー検索に失敗しました')
       })
 
-      function buildAddedUser(user){
+      function buildAddedUser(user_name, user_id){
         var html_add_user = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                          <input name='group[user_ids][]' type='hidden' value=${user.id}>
-                         <p class='chat-group-user__name'>${user.name}</p>
+                          <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                         <p class='chat-group-user__name'>${user_name}</p>
                          <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                          </div>`
         return html_add_user;
       }
 
-      $(document).on('click', '.chat-group-user__btn--add', function(){
-        // console.log("ok")
-        // var input = $(this).val();
-         
-        $.ajax({ 
-          url:'/users', 
-          type: 'GET',
-          data: ('keyword=' + input),
-          processData: false, 
-          contentType: false,
-          dataType: 'json'
-      })
-      .done(function(user){
-        console.log(user);
-        var html_add_user =buildAddedUser(user);
+      $(document).on('click', '.chat-group-user__btn--add', function(user_name, user_id){
+        // console.log(user_name, user_id);
+        var user_name = $(this).data("user-name");
+        var user_id = $(this).data("user-id");
+        var html_add_user =buildAddedUser(user_name, user_id);
         $("#chat-group-users").append(html_add_user);
-        })
-      
-      .fail(function(){
-        alert('メンバー追加に失敗しました')
+        $(this).parent().remove();
+        })   
+  })
+      $(document).on('click', '.chat-group-user__btn--remove', function(html_add_user){
+        $("#chat-group-users").remove(html_add_user);
       })
-      })
-  });
 });
